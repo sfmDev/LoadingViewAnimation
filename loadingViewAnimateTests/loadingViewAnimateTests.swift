72b00c3a27a272a7d3aab3c979fbe25783efb09c
石峰铭 <sfm.shi@5253w.com>
2016-05-28 13:30:10 +0800

    Initial Commit

---
+++ loadingViewAnimateTests/loadingViewAnimateTests.swift
@@ -0,0 +1,36 @@
+//
+//  loadingViewAnimateTests.swift
+//  loadingViewAnimateTests
+//
+//  Created by 石峰铭 on 16/5/28.
+//  Copyright © 2016年 shifengming. All rights reserved.
+//
+
+import XCTest
+@testable import loadingViewAnimate
+
+class loadingViewAnimateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
